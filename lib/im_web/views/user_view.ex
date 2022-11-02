6d0b4e0327fc902b7fd6d974b36f0706ab68732d
Manuel Luna <manuellunada@gmail.com>
2022-11-01 18:56:25 -0600

    view

--- lib/im_web/views/user_view.ex
+++ lib/im_web/views/user_view.ex
@@ -3,18 +3,17 @@ defmodule ImWeb.UserView do
   alias ImWeb.UserView
 
   def render("index.json", %{users: users}) do
-    %{data: render_many(users, UserView, "user.json")}
+    render_many(users, UserView, "user.json")
   end
 
   def render("show.json", %{user: user}) do
-    %{data: render_one(user, UserView, "user.json")}
+    render_one(user, UserView, "user.json")
   end
 
   def render("user.json", %{user: user}) do
     %{
       id: user.id,
-      username: user.username,
-      password: user.password
+      username: user.username
     }
   end
 end
